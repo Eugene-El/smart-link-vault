b811ab515a5d6c9e494133b02b1dafbbfe2ef8af
El <lomashonok@inbox.lv>
2020-06-22 11:31:07 +0300

    Structure of page content styles and scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   styleUrls: ['./app.component.css'],
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
+
+  constructor( private cdref: ChangeDetectorRef ) {}
+  
+  ngAfterContentChecked() {
+
+    this.methods.getHeight();
+    this.cdref.detectChanges();
+    
+  }
+
+  options = {
+    routerHeight: 0 as number
+  }
+
+  methods = {
+    getHeight: () : void => {
+      let height = window.innerHeight;
+      let menu = document.getElementsByTagName("slv-menu")[0];
+      if (menu != null) {
+        height -= menu.clientHeight;
+      }
+      console.log(height);
+      this.options.routerHeight = height;
+    }
+  }
+
 }
